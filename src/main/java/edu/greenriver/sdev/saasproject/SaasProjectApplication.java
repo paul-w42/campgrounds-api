6f8dbfe7438dab8ad7f3704cbbb0177d44409bc6
Paul Woods <paul.n.woods@gmail.com>
2023-10-03 14:21:26 -0700

    Initial commit for saas-project.

---
+++ src/main/java/edu/greenriver/sdev/saasproject/SaasProjectApplication.java
@@ -0,0 +1,13 @@
+package edu.greenriver.sdev.saasproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaasProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaasProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/greenriver/sdev/saasproject/SaasProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.greenriver.sdev.saasproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaasProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
